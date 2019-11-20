876cfe31b3081fa3c952c39c6b5014c3b43496a7
Natalia Ciosek <n_ciosek@wp.pl>
2019-11-20 09:33:53 +0100

    login page without validations

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
 
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
-import { MustMatch } from 'src/app/_helpers/must-much.validator';
 
 @Component({
   selector: 'app-login',
@@ -14,29 +12,8 @@ export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     submitted = false;
 
-
   constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-  }, {
-      validator: MustMatch('password', 'confirmPassword')
-  });
-  }
-
-  get l() { return this.loginForm.controls; }
-
-  onSubmit() {
-    this.submitted = true;
-
-    // stop here if form is invalid
-    if (this.loginForm.invalid) {
-        return;
-    }
-
-    alert('SUCCESS!!')
-}
+  ngOnInit() {  }
 
 }
